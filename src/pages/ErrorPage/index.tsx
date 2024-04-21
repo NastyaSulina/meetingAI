00a3b1623e60d58a75090ff3e7ec6a144ec75c29
imgrau <sulina01@mail.ru>
2024-04-21 20:10:53 +0300

    error page

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
 import styles from './ErrorPage.module.scss'
+import { Figure, FigureType, Eye, Text, TextType } from '../../shared/components'
+import { Menu } from '../../widgets'
 
 const ErrorPage = () => {
-    return <div className={styles.root}>Попалась ошибка!</div>
+    return (
+        <>
+            <Menu />
+
+            <div className={styles.wrapper}>
+                <Text
+                    text='Попалась ошибка! Такой страницы не существует...'
+                    textType={TextType.subhead}
+                />
+                <div className={styles.figure}>
+                    <Figure
+                        figureType={FigureType.STAR}
+                        width={212}
+                        height={212}
+                        fill='var(--Orange)'
+                    >
+                        <div className={styles.eye}>
+                            <Eye />
+                        </div>
+                    </Figure>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default ErrorPage
